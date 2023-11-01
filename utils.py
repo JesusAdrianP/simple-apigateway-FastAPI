b7a56c97f14d0fa02f601bdc464aeb4eace15856
juanBailon <juanojo2001.5@gmail.com>
2023-10-31 20:46:35 -0500

    documenting utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 import json
-
-
+from trie import Trie
 
 
 def get_data_from_json_file(file_path: str) -> dict[str, dict]:
+    """ Load JSON data from a file and return it as a dictionary. """
     
     with open(file_path, 'r') as file:
         data = json.load(file)
@@ -12,23 +12,56 @@ def get_data_from_json_file(file_path: str) -> dict[str, dict]:
 
 
 def resorce_path_identifier_to_service_map(urls_config: dict) -> dict[str, str]:
+    """ Creates a mapping of resource path identifiers to corresponding service names. """
     
     return {value["resource_path_identifier"]: key for key, value in urls_config.items()}
 
 
+def generate_trie_for_resorce_path_identifiers(urls_config: dict) -> Trie:
+    """ Generate a Trie data structure for resource path identifiers. """
+
+    trie = Trie()
+    for service_data in urls_config.values():
+
+        resorce_path = service_data["resource_path_identifier"]     
+        trie.insert(resorce_path)
+
+    return trie
+
+        
 
 def resolve_mircroservice_url(target_url: str,
                               resources_services_map: dict[str, str],
-                              urls_config: dict[str, dict]
+                              urls_config: dict[str, dict],
+                              url_path_trie: Trie
                               ) -> str | None:
-    
-    url_parts = target_url.split('/')
-    url_parts = url_parts[1:]
+    """ 
+    Resolve a microservice URL based on the target URL.
 
-    path_identifier = url_parts[0]
+    If i client makes a reques to 
+        http://gateway-url.com/users/api/token
+    
+    Then the values pass in the target_ulr should be
+        /users/api/token
+
+    
+    Then return values will be the service url that matches the pattern
+    in the target_url, so if there is a service that does this, the fucntion
+    will return something like this.
+
+        http://some-service.com/users/api/token
+
+    In case that none of the service int he ulrs_config mathces the target url
+    the fucntion will return None
+
+    """
+    
+    search_result = url_path_trie.longest_matched_string(target_url)
+
     try:
-        print(path_identifier)
-        service_name = resources_services_map[path_identifier]
+        print(target_url)
+        service_name = resources_services_map[search_result]
+    
     except KeyError:
         return None
 
@@ -37,19 +70,3 @@ def resolve_mircroservice_url(target_url: str,
 
     return service_url + target_url
 
-
-        
-# URLS_JSON_FILE = './urls_config.json'
-
-# URLS_CONFIG = get_data_from_json_file(URLS_JSON_FILE)
-
-# RESORCES_TO_SERVICES = resorce_path_identifier_to_service_map(URLS_CONFIG)
-    
-# url = resolve_mircroservice_url(target_url='/users/api/token',
-#                                     resources_services_map= RESORCES_TO_SERVICES,
-#                                     urls_config= URLS_CONFIG
-#                                     )
-
-    
-
-# print(url)
