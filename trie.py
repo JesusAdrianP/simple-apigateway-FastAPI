d238afbf908018161228f69de61c20dde1770990
juanBailon <juanojo2001.5@gmail.com>
2023-10-31 20:35:01 -0500

    documenting trie.py

--- trie.py
+++ trie.py
@@ -2,10 +2,13 @@ from typing import Optional
 
 
 class Trie:
+    """ Class to represent a Trie data structure"""
+
     def __init__(self):
         self.root = self.Node()
 
     def insert(self, s: str) -> None:
+        """ inserst a new string to the trie """
         node = self.root
         for ch in s:
             if ch not in node.children:
@@ -14,6 +17,12 @@ class Trie:
         node.is_end = True
 
     def search(self, s: str) -> Optional['Trie.Node']:
+        """ 
+        Finds if the input string is part of the Trie, if its part of the Trie
+        returns the trie node that contains the last elemet of the provided input string. 
+        
+        Otherwise returns None
+        """
         node = self.root
         for ch in s:
             if ch not in node.children:
@@ -22,7 +31,9 @@ class Trie:
         return node if node.is_end else None
     
 
-    def get_strings(self):
+    def get_strings(self) -> list[str]:
+        """ Returns a list containing all the strings that are part of the trie """
+
         def rec(node: 'Trie.Node', string: list[str], all_strings: list[str]) -> list[str]:
             if node.is_end:
                 all_strings.append("".join(string))
@@ -38,8 +49,39 @@ class Trie:
         return all_strings
     
 
+    def longest_matched_string(self, s: str) -> Optional[str]:
+        """
+        Find the longest matching string in the Trie for the input string.
+
+        example: 
+            Lets say the Trie has this strings
+                '/users' and '/users/api'
+
+            if the input of the method is 
+                '/users/api/token'
+            Then the return value will be 
+                '/users/api'
+
+        """
+
+        node = self.root
+        longest_match = None
+        matched_string = ""
+
+        for ch in s:
+            if ch not in node.children:
+                break
+            node = node.children[ch]
+            matched_string += ch
+            if node.is_end:
+                longest_match = matched_string
+
+        return longest_match
+    
+
 
     class Node:
+        """ Node class for the Trie data structure """
         def __init__(self, is_end=False):
             self.children = {}
             self.is_end = is_end
