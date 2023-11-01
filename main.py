b9ff951bb1c3f227558e71f82b8c0ca777f76fb1
juanBailon <juanojo2001.5@gmail.com>
2023-11-01 02:20:29 -0500

    changing the path of URLS_JSON_FILE to use os.path to ensure cross-platform compatibility

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from proxy_manager import reverse_proxy
 from  utils import get_data_from_json_file, resorce_path_identifier_to_service_map, generate_trie_for_resorce_path_identifiers
@@ -5,7 +6,7 @@ from  utils import get_data_from_json_file, resorce_path_identifier_to_service_m
 
 app = FastAPI()
 
-URLS_JSON_FILE = './urls_config.json'
+URLS_JSON_FILE = os.path.join('.', 'urls_config.json')
 
 URLS_CONFIG = get_data_from_json_file(URLS_JSON_FILE)
 
