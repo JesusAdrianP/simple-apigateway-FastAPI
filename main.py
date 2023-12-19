82bc1bec5dad1e93dea667bcb9296811fb6fa1b3
JesusAdrianP <95390821+JesusAdrianP@users.noreply.github.com>
2023-12-19 15:18:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from  utils import get_data_from_json_file, resorce_path_identifier_to_service_m
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 URLS_JSON_FILE = os.path.join('.', 'urls_config.json')
 
 URLS_CONFIG = get_data_from_json_file(URLS_JSON_FILE)
@@ -13,14 +21,6 @@ URLS_CONFIG = get_data_from_json_file(URLS_JSON_FILE)
 RESORCES_TO_SERVICES = resorce_path_identifier_to_service_map(URLS_CONFIG)
 
 URL_PATHS_TRIE = generate_trie_for_resorce_path_identifiers(URLS_CONFIG)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Puedes reemplazar "*" con la URL específica de tu frontend en Render
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def read_root():
